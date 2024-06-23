650770c7b932bcb46b9fbc3efc5bf68574b9a36a
imran <87297004+imranshaikh9930@users.noreply.github.com>
2024-06-23 12:45:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,46 @@
 Array.prototype.myMap = function(callback) {
+
+	const res = [];
+	for(let i = 0 ; i< this.length;i++){
+		res.push(callback(this[i],i,this));
+	}
+
+	return res;
+	
+	
     
   };
   
   Array.prototype.myFilter = function(callback) {
+
+	  const res = [];
+	  for(let i  = 0 ; i< this.length;i++){
+		  if(callback(this[i] , i , this)){
+			  res.push(this[i]);
+		  }
+	  }
+	  return res;
   
   };
   
   Array.prototype.myReduce = function(callback, initialValue) {
 
+	  let accumlator = initialValue;
+	  let startIndex = 0;
+
+	  if(accumlator === undefined){
+		  if(this.length === 0){
+			  throw new TypeError("Reduce OF empty array");
+		  }
+
+		  accumlator = this[0];
+		  startIndex = 1;
+	  }
+
+	  for(let i = startIndex; i< this.length;i++){
+		  accumlator = callback(accumlator,this[i],i,this);
+	  }
+	  return accumlator;
   };
   
   
